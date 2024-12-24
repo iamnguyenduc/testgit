1c0951eb3eb8c556f220674fbf1167570dabecad
iamnguyenduc <85486907+iamnguyenduc@users.noreply.github.com>
2024-12-24 10:43:43 +0700

    fe

--- MyRazorProject/obj/Debug/net8.0/MyRazorProject.AssemblyInfo.cs
+++ MyRazorProject/obj/Debug/net8.0/MyRazorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd130a65e353766bef4771e0ec8518e40532859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c32eb3422bc077ba35f99a2951732e09df227af")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
